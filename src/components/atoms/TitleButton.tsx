9f504bad069e903338ce3be925cb3210bd7c10ce
natsumesou <natsumesouxx@gmail.com>
2021-04-22 17:31:08 +0900

    タイトルをリンクに変更

---
+++ src/components/atoms/TitleButton.tsx
@@ -0,0 +1,12 @@
+import { Button, Typography } from '@material-ui/core'
+import React from 'react'
+
+export const TitleButton = () => {
+  return (
+    <Button href="/">
+      <Typography variant="h1">
+        Yoshiaki Natsume
+      </Typography>
+    </Button>
+  )
+}
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,20 +1,19 @@
-import { AppBar, Button, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Toolbar } from '@material-ui/core'
 import React from 'react'
 import { StaticImage } from "gatsby-plugin-image"
+import { TitleButton } from "../atoms/TitleButton"
 
 const style: React.CSSProperties = {
   width: '100%',
   height: 'calc(100vw/3)',
 };
 
-export const Header = (classes) => {
+export const Header = () => {
   return (
     <header>
     <AppBar position="sticky">
       <Toolbar variant="regular">
-      <Typography variant="h1" className={classes.title}>
-        Yoshiaki Natsume
-      </Typography>
+        <TitleButton />
       </Toolbar>
     </AppBar>
     <StaticImage src="../../images/header.webp" alt="header image" objectFit={ 'cover' } style={ style } />
